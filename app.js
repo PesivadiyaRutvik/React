230f48265507311a7e0b5877cc2064875b2c8308
Rutvik Pesivadiya <pesivadiyarutvik@gmail.com>
2024-01-02 23:36:41 +0530

    feat(react-element): Hellow wolrd from react element

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const heading = React.createElement('h1', {}, "Hello Wolrd!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const heading = <h1>Hello Woerld form reactElement!</h1>;
 
 root.render(heading);
