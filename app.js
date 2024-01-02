718cc6c48ca14ec5dee636a9af2636df701be01a
Rutvik Pesivadiya <pesivadiyarutvik@gmail.com>
2024-01-02 23:45:22 +0530

    feat(react-component-composition): Render the component using react functional component

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const heading = <h1>Hello Woerld form reactElement!</h1>;
+const Heading = () => <h1>Hello Woerld form react compoennt!</h1>;
 
-root.render(heading);
+const HeadingCompoent = () => {
+    return (
+        <div id="parent">
+            <Heading/>
+            <div id="child">This is child</div>
+        </div>)
+}
+
+root.render(<HeadingCompoent />);
